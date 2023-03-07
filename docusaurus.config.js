f982e619c3b808bc91c861fd3f3d31c670c439e7
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-07 16:13:37 +0300

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,12 +6,12 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "My Site",
+  title: "Interview Notes",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "interview-8flverd23-tolgatopkac.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
