4e9e84b2cd14abed5286112971d7de6a15ee16c6
tolgatopkac-gm <72669663+tolgatopkac@users.noreply.github.com>
2023-03-07 16:14:47 +0300

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "interview-8flverd23-tolgatopkac.vercel.app",
+  url: "https://interview-8flverd23-tolgatopkac.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
